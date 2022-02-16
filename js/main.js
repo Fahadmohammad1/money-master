88bd3346e9607b1f49b0debcb3db09fdf5c0d505
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-02-17 03:37:39 +0600

    expences ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+// expencess calculate
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const foodInput = document.getElementById("food-input");
+  const foodCost = parseFloat(foodInput.value);
+  const rentInput = document.getElementById("rent-input");
+  const rentCost = parseFloat(rentInput.value);
+  const clothInput = document.getElementById("cloth-input");
+  const clothCost = parseFloat(clothInput.value);
 
+  const expencessInput = document.getElementById("expences-total");
+  const expencesTotal = foodCost + rentCost + clothCost;
+  expencessInput.innerText = expencesTotal;
+
+  const incomeInput = document.getElementById("income-input");
+  const totalIncome = parseFloat(incomeInput.value);
+
+  const calcBalance = totalIncome - parseFloat(expencessInput.innerText);
+  //
+
+  const balanceInput = document.getElementById("balance-total");
+  const balanceText = parseFloat(balanceInput.innerText);
+
+  const availableBalance = parseFloat(balanceText) + parseFloat(calcBalance);
+  balanceInput.innerText = availableBalance;
+});
