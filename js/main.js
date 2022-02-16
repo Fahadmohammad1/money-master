cee547de997d86c79d0974de843eb4b202380dff
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-02-17 04:04:35 +0600

    one function almost

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-// expencess calculate
-document.getElementById("calculate-btn").addEventListener("click", function () {
-  const foodInput = document.getElementById("food-input");
+function getInput(spent) {
+  const foodInput = document.getElementById(spent + "-input");
   const foodCost = parseFloat(foodInput.value);
-  const rentInput = document.getElementById("rent-input");
-  const rentCost = parseFloat(rentInput.value);
-  const clothInput = document.getElementById("cloth-input");
-  const clothCost = parseFloat(clothInput.value);
 
   const expencessInput = document.getElementById("expences-total");
   const expencesTotal = foodCost + rentCost + clothCost;
   expencessInput.innerText = expencesTotal;
+}
+
+// expencess calculate
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  getInput("food");
+  getInput("rent");
+  getInput("cloth");
+  //   const foodInput = document.getElementById("food-input");
+  //   const foodCost = parseFloat(foodInput.value);
+  //   const rentInput = document.getElementById("rent-input");
+  //   const rentCost = parseFloat(rentInput.value);
+  //   const clothInput = document.getElementById("cloth-input");
+  //   const clothCost = parseFloat(clothInput.value);
+
+  //   const expencessInput = document.getElementById("expences-total");
+  //   const expencesTotal = foodCost + rentCost + clothCost;
+  //   expencessInput.innerText = expencesTotal;
 
   const incomeInput = document.getElementById("income-input");
   const totalIncome = parseFloat(incomeInput.value);
@@ -22,4 +34,5 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
 
   const availableBalance = parseFloat(balanceText) + parseFloat(calcBalance);
   balanceInput.innerText = availableBalance;
+  incomeInput.value = 0;
 });
